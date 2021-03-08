d163c2fe418ed680805b0f2893cc289d587f627a
Upamanyu Sharma <upamanyu@mit.edu>
2021-03-08 14:05:52 -0500

    Get rid of unnecessary code

--- aof.go
+++ aof.go
@@ -3,10 +3,7 @@ package gokv
 import (
 	"os"
 	"syscall"
-	// "fmt"
 	"sync"
-	// "time"
-	// "math/rand"
 )
 
 type AppendableFile struct {
@@ -34,30 +31,23 @@ func CreateAppendableFile(fname string) *AppendableFile {
 
 	go func() {
 		a.mu.Lock()
-		// numSyncs := 0
-		// lastWritten := uint64(0)
 		for {
 			if len(a.membuf) == 0 {
 				a.lengthCond.Wait()
 				continue
 			}
 
-			// if uint64(len(a.membuf)) < 3 * lastWritten / 4 {
-			// a.mu.Unlock()
-			// time.Sleep(time.Duration((rand.Uint64() % 500)) * time.Microsecond)
-			// a.mu.Lock()
-			// }
 			l := a.membuf
-			a.membuf = make([]byte, 65536/2)
+			a.membuf = make([]byte, 0)
 			a.mu.Unlock()
 
+			// TODO: replace this with a AtomicAppend() call
 			a.f.Write(append(l))
 			syscall.Fdatasync(int(a.f.Fd()))
 
 			a.mu.Lock()
 			a.durableLength += uint64(len(l))
 			a.durableCond.Broadcast()
-			// time.Sleep(time.Duration(rand.Uint64() % 500) * time.Microsecond)
 		}
 	}()
 
@@ -74,7 +64,6 @@ func (a *AppendableFile) Append(data []byte) uint64 {
 }
 
 func (a *AppendableFile) WaitAppend(length uint64) {
-	// time.Sleep(time.Duration((rand.Uint64() % 20)) * time.Microsecond)
 	a.mu.Lock()
 	for a.durableLength < length {
 		a.durableCond.Wait()
--- gokv.go
+++ gokv.go
@@ -2,7 +2,6 @@ package gokv
 
 import (
 	"github.com/tchajed/marshal"
-	"io/ioutil"
 	"sync"
 )
 
@@ -11,7 +10,6 @@ type GoKVServer struct {
 	lastReply map[uint64]uint64
 	lastSeq   map[uint64]uint64
 	kvs       map[uint64]string
-	kvsSize   uint64
 	durable   bool
 	opLog     *AppendableFile
 }
@@ -21,21 +19,7 @@ type PutArgs struct {
 	Value string
 }
 
-// XXX: for correctness under packet duplication, should have generation number
-func (s *GoKVServer) ResetRPC(args *struct{}, reply *struct{}) error {
-	s.mu.Lock()
-
-	s.lastReply = make(map[uint64]uint64)
-	s.lastSeq = make(map[uint64]uint64)
-	s.kvs = make(map[uint64]string)
-	s.kvsSize = 8
-	WriteDurableKVServer(s)
-
-	s.mu.Unlock()
-	return nil
-}
-
-func (s *GoKVServer) appendPut(args *PutArgs) uint64 {
+func encodePut(args *PutArgs) []byte {
 	v := []byte(args.Value)
 	num_bytes := uint64(8 + 8 + len(v)) // key + value-len + value
 	e := marshal.NewEnc(num_bytes)
@@ -43,30 +27,15 @@ func (s *GoKVServer) appendPut(args *PutArgs) uint64 {
 	e.PutInt(uint64(len(v)))
 	e.PutBytes(v)
 
-	return s.opLog.Append(e.Finish())
-	// return s.opLog.Append([]byte(fmt.Sprintf("%d\n%d\n%s", args.Key, len(args.Value), args.Value)))
+	return e.Finish()
 }
 
 func (s *GoKVServer) PutRPC(args *PutArgs, reply *struct{}) error {
 	s.mu.Lock()
-	// fmt.Println(atomic.LoadInt32(puts))
-	oldv, ok := s.kvs[args.Key]
-	if ok {
-		s.kvsSize -= uint64(len([]byte(oldv)))
-		s.kvsSize += uint64(len([]byte(args.Value)))
-	} else {
-		s.kvsSize += uint64(len([]byte(args.Value)))
-		s.kvsSize += 8
-		s.kvsSize += 8
-	}
 	s.kvs[args.Key] = args.Value
-	if s.durable {
-		l := s.appendPut(args)
-		s.mu.Unlock()
-		s.opLog.WaitAppend(l)
-		return nil
-	}
+	l := s.opLog.Append(encodePut(args))
 	s.mu.Unlock()
+	s.opLog.WaitAppend(l)
 	return nil
 }
 
@@ -77,35 +46,7 @@ func (s *GoKVServer) GetRPC(key *uint64, value *string) error {
 	return nil
 }
 
-func EncMap(e *marshal.Enc, m map[uint64]uint64) {
-	e.PutInt(uint64(len(m)))
-	for key, value := range m {
-		e.PutInt(key)
-		e.PutInt(value)
-	}
-}
-
-// requires ?some? amount of space
-func EncByteMap(e *marshal.Enc, m map[uint64]string) {
-	e.PutInt(uint64(len(m)))
-	for key, value := range m {
-		e.PutInt(key)
-		e.PutInt(uint64(len(value)))
-		e.PutBytes([]byte(value))
-	}
-}
-
-func WriteDurableKVServer(ks *GoKVServer) {
-	num_bytes := uint64(8*(2*len(ks.lastSeq)+2*len(ks.lastReply)+2)) + ks.kvsSize
-	e := marshal.NewEnc(num_bytes) // 4 uint64s
-	EncMap(&e, ks.lastSeq)
-	EncMap(&e, ks.lastReply)
-	EncByteMap(&e, ks.kvs)
-
-	// TODO: this isn't crash-atomic
-	ioutil.WriteFile("kvdur", e.Finish(), 0644)
-	return
-}
+// TODO: recovery
 
 func MakeGoKVServer() *GoKVServer {
 	srv := new(GoKVServer)
@@ -113,8 +54,6 @@ func MakeGoKVServer() *GoKVServer {
 	srv.lastReply = make(map[uint64]uint64)
 	srv.lastSeq = make(map[uint64]uint64)
 	srv.kvs = make(map[uint64]string)
-	srv.kvsSize = 8 // for len
-	srv.durable = true
 	srv.opLog = CreateAppendableFile("kvdur_log")
 	return srv
 }
