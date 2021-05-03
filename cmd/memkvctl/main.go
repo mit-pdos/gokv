e5e6f7de8f9b48bb6084cfba8a6b86f55114050b
Upamanyu Sharma <upamanyu@mit.edu>
2021-05-03 19:21:23 -0400

    Added ctl utility

---
+++ cmd/memkvctl/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/mit-pdos/gokv/memkv"
+	"github.com/mit-pdos/gokv/dist_ffi"
+	"flag"
+	"strconv"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var coordStr string
+	flag.StringVar(&coordStr, "coord", "", "address of coordinator")
+	flag.Parse()
+
+	usage_assert := func(b bool) {
+		if !b {
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
+	}
+
+	usage_assert(coordStr != "")
+
+	coord := dist_ffi.MakeAddress(coordStr)
+	ck := memkv.MakeMemKVClerk(coord)
+
+	a := flag.Args()
+	usage_assert(len(a) == 0)
+	if a[0] == "get" {
+		usage_assert(len(a) == 2)
+		k, err := strconv.ParseUint(a[1], 10, 64)
+		usage_assert(err == nil)
+		v := ck.Get(k)
+		fmt.Println("GET %d |-> %v", k, v)
+	} else if a[0] == "put" {
+		usage_assert(len(a) == 3)
+		k, err := strconv.ParseUint(a[1], 10, 64)
+		usage_assert(err == nil)
+		v := []byte(a[2])
+		ck.Put(k, v)
+		fmt.Println("PUT %d |-> %v", k, v)
+	} else if a[0] == "add" {
+		usage_assert(len(a) == 2)
+		h := dist_ffi.MakeAddress(a[1])
+		ck.Add(h)
+		fmt.Println("Added %s", a[1])
+	}
+}
