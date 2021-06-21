5998e31e562950230788879e1d20bf9f3491f6f1
Upamanyu Sharma <upamanyu@mit.edu>
2021-06-21 09:51:16 -0400

    Added clerk for mkrouter

---
+++ mkrouter/clerk.go
@@ -0,0 +1,21 @@
+package mkrouter
+
+import (
+	"github.com/mit-pdos/gokv/memkv"
+	"github.com/mit-pdos/gokv/urpc/rpc"
+)
+
+type Clerk struct {
+	cl *rpc.RPCClient
+}
+
+func (ck *Clerk) Get(key uint64) []byte {
+	var ret []byte
+	ck.cl.Call(memkv.KV_GET, memkv.EncodeGetRequest(&memkv.GetRequest{Key:key} ), &ret, 100/*ms*/)
+	return ret
+}
+
+func (ck *Clerk) Put(key uint64, value []byte) {
+	var ret []byte
+	ck.cl.Call(memkv.KV_GET, memkv.EncodePutRequest(&memkv.PutRequest{Key:key, Value:value} ), &ret, 100/*ms*/)
+}
