c469966408d1b15315f6925d05de41ed90531ddb
Upamanyu Sharma <upamanyu@mit.edu>
2021-07-23 10:33:52 -0400

    Add port to rpcscale

--- cmd/rpcscale/main.go
+++ cmd/rpcscale/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/felixge/fgprof"
 	"github.com/mit-pdos/gokv/grove_ffi"
@@ -9,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
 )
 
 const (
@@ -17,6 +19,14 @@ const (
 )
 
 func main() {
+	var port uint64
+	flag.Uint64Var(&port, "port", 0, "port number to user for server")
+	flag.Parse()
+	if port == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	http.DefaultServeMux.Handle("/debug/fgprof", fgprof.Handler())
 	go func() {
 		log.Println(http.ListenAndServe("localhost:6060", nil))
@@ -36,8 +46,8 @@ func main() {
 	}
 
 	s := rpc.MakeRPCServer(handlers)
-	me := grove_ffi.MakeAddress(fmt.Sprintf("0.0.0.0:%d", 12345))
-	log.Printf("Started null RPC server on port %d; id %d", 12345, me)
+	me := grove_ffi.MakeAddress(fmt.Sprintf("0.0.0.0:%d", port))
+	log.Printf("Started null RPC server on port %d; id %d", port, me)
 	s.Serve(me, 1)
 	select {}
 }
