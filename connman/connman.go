ffbb56672c7a1e3bfef58fbb7de69793ba427bd3
Ralf Jung <post@ralfj.de>
2021-09-01 14:09:06 -0400

    slight internal refactor of ConnMan

--- connman/connman.go
+++ connman/connman.go
@@ -17,40 +17,46 @@ type ConnMan struct {
 	making map[HostName]*sync.Cond // a key exists iff someone is making the RPCClient for that host right now
 }
 
-func (c *ConnMan) getNewClient(host HostName) *rpc.RPCClient {
-	// want to open a new RPCClient without a thundering herd of threads all
-	// making their own RPCClient
-	var cl *rpc.RPCClient
+func (c *ConnMan) getClient(host HostName) *rpc.RPCClient {
+	var ret *rpc.RPCClient
 
-	// XXX: This is written under the assumption that MakeRPCClient
-	// takes a long time compared to the other critical sections of c.mu
-	// (e.g. this might establish a TCP connection, incurring some
-	// network delay)
-	cond, ok := c.making[host];
-	if ok { // someone else is making the host
-		cond.Wait()
-		cl = c.rpcCls[host]
-	} else {
-		c.making[host] = sync.NewCond(c.mu)
-		c.mu.Unlock()
-		cl = rpc.MakeRPCClient(host)
-		c.mu.Lock()
-		c.rpcCls[host] = cl
-		c.making[host].Broadcast()
-		delete(c.making, host)
+	c.mu.Lock()
+	for {
+		cl, ok := c.rpcCls[host]
+		if ok {
+			ret = cl
+			break
+		} else {
+			// want to open a new RPCClient without a thundering herd of threads all
+			// making their own RPCClient
+			// XXX: This is written under the assumption that MakeRPCClient
+			// takes a long time compared to the other critical sections of c.mu
+			// (e.g. this might establish a TCP connection, incurring some
+			// network delay)
+			cond, ok := c.making[host];
+			if ok { // someone else is making the host
+				cond.Wait()
+				continue
+			} else {
+				c.making[host] = sync.NewCond(c.mu)
+				c.mu.Unlock()
+				ret = rpc.MakeRPCClient(host)
+				c.mu.Lock()
+				c.rpcCls[host] = ret
+				c.making[host].Broadcast()
+				delete(c.making, host)
+				break
+			}
+		}
 	}
-	return cl
+	c.mu.Unlock()
+	return ret
 }
 
 // This repeatedly retries the RPC after retryTimeout until it gets a response.
 func (c *ConnMan) CallAtLeastOnce(host HostName, rpcid uint64, args []byte, reply *[]byte, retryTimeout uint64) {
 	var cl *rpc.RPCClient
-	c.mu.Lock()
-	cl, ok := c.rpcCls[host]
-	if !ok {
-		cl = c.getNewClient(host)
-	}
-	c.mu.Unlock()
+	cl = c.getClient(host)
 
 	for {
 		err := cl.Call(rpcid, args, reply, retryTimeout)
@@ -60,13 +66,11 @@ func (c *ConnMan) CallAtLeastOnce(host HostName, rpcid uint64, args []byte, repl
 		} else if err == rpc.ErrDisconnect {
 			// need to try reconnecting
 			c.mu.Lock()
-			if cl != c.rpcCls[host] { // our RPCClient is already out of date
-				cl = c.rpcCls[host]
-			} else {
+			if cl == c.rpcCls[host] { // our RPCClient might already be out of date
 				delete(c.rpcCls, host)
-				cl = c.getNewClient(host)
 			}
 			c.mu.Unlock()
+			cl = c.getClient(host)
 			continue
 		} else {
 			break
