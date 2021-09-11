83a5698c8a704992b80a3d5ac4acb6946e655eb1
Ralf Jung <post@ralfj.de>
2021-09-11 16:03:49 -0400

    make it Goose (no more interfaces)

--- memkv/memkv_concurrent_clerk.go
+++ memkv/memkv_concurrent_clerk.go
@@ -5,35 +5,29 @@ import (
 	"github.com/goose-lang/std"
 )
 
-type KVClerk interface {
-	Put(uint64, []byte)
-	Get(uint64) []byte
-}
-
 type KVClerkPool struct {
 	mu *sync.Mutex
 	// queue of free clerks
-	freeClerks []KVClerk
-	factory    func() KVClerk
+	freeClerks []*MemKVClerk
+	coord HostName
 }
 
-// the hope is that after a while, the number of clerks needed to maintain a
-// request rate for an open system benchmark will stabilize.
-func (p *KVClerkPool) Put(key uint64, value []byte) {
+func (p *KVClerkPool) getClerk() *MemKVClerk {
 	p.mu.Lock()
-	var ck KVClerk
+	var ck *MemKVClerk
 	if len(p.freeClerks) == 0 {
-		ck = p.factory()
+		p.mu.Unlock() // don't want to hold lock while making a fresh clerk
+		ck = MakeMemKVClerk(p.coord)
 	} else {
 		ck = p.freeClerks[0]
 		p.freeClerks = p.freeClerks[1:]
+		p.mu.Unlock()
 	}
-	p.mu.Unlock()
+	return ck
+}
 
-	// we now own ck
-	ck.Put(key, value)
-
-	// done with ck, so asynchronously put it back in the free list
+func (p *KVClerkPool) putClerk(ck *MemKVClerk) {
+	// asynchronously put it back in the free list
 	go func() {
 		p.mu.Lock()
 		p.freeClerks = append(p.freeClerks, ck)
@@ -41,24 +35,26 @@ func (p *KVClerkPool) Put(key uint64, value []byte) {
 	}()
 }
 
+// the hope is that after a while, the number of clerks needed to maintain a
+// request rate for an open system benchmark will stabilize.
+func (p *KVClerkPool) Put(key uint64, value []byte) {
+	ck := p.getClerk()
+
+	// we now own ck
+	ck.Put(key, value)
+
+	// done with ck, so asynchronously put it back in the free list
+	p.putClerk(ck)
+}
+
 func (p *KVClerkPool) Get(key uint64) []byte {
-	p.mu.Lock()
-	var ck KVClerk
-	if len(p.freeClerks) == 0 {
-		p.mu.Unlock() // don't want to hold lock while making a fresh clerk
-		ck = p.factory()
-	} else {
-		ck = p.freeClerks[0]
-		p.freeClerks = p.freeClerks[1:]
-		p.mu.Unlock()
-	}
+	ck := p.getClerk()
 
 	// we now own ck
 	value := ck.Get(key)
 
-	p.mu.Lock()
-	p.freeClerks = append(p.freeClerks, ck)
-	p.mu.Unlock()
+	p.putClerk(ck)
+
 	return value
 }
 
@@ -73,13 +69,13 @@ func (p *KVClerkPool) MGet(keys []uint64) [][]byte {
 	return vals
 }
 
-func MakeKVClerkPool(numInit uint64, numClients uint64, factory func() KVClerk) *KVClerkPool {
+func MakeKVClerkPool(numInit uint64, coord HostName) *KVClerkPool {
 	p := new(KVClerkPool)
 	p.mu = new(sync.Mutex)
-	p.factory = factory
-	p.freeClerks = make([]KVClerk, numInit)
+	p.coord = coord
+	p.freeClerks = make([]*MemKVClerk, numInit)
 	for i := uint64(0); i < numInit; i++ {
-		p.freeClerks[i] = p.factory()
+		p.freeClerks[i] = MakeMemKVClerk(p.coord)
 	}
 
 	return p
