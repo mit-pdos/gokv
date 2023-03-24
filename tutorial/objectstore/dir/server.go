b0eaf18fc11ecc186813ce94688854b2ebed2044
Upamanyu Sharma <upamanyu@mit.edu>
2023-03-24 15:52:40 -0400

    Simplified dir data representation

--- tutorial/objectstore/dir/server.go
+++ tutorial/objectstore/dir/server.go
@@ -9,17 +9,21 @@ import (
 
 type WriteID = uint64
 
-type Value struct {
+type PartialValue struct {
 	// map from chunk index to where the data lives
 	servers map[uint64]ChunkHandle
 	// TODO: maybe also track length as metadata?
 }
 
+type Value struct {
+	// list of where each chunk lives
+	servers []ChunkHandle
+}
+
 type Server struct {
 	m *sync.Mutex
 	// ongoing can store partial values (some indices are missing)
-	ongoing map[WriteID]Value
-	// these values are always complete
+	ongoing     map[WriteID]PartialValue
 	data        map[string]Value
 	nextWriteId WriteID
 }
@@ -89,7 +93,7 @@ func ParsePreparedRead(data []byte) PreparedRead {
 func StartServer(me grove_ffi.Address) {
 	s := &Server{
 		m:           new(sync.Mutex),
-		ongoing:     make(map[WriteID]Value),
+		ongoing:     make(map[WriteID]PartialValue),
 		data:        make(map[string]Value),
 		nextWriteId: 1,
 	}
@@ -122,7 +126,7 @@ func (s *Server) PrepareWrite() PreparedWrite {
 	s.m.Lock()
 	id := s.nextWriteId
 	s.nextWriteId += 1
-	s.ongoing[id] = Value{servers: make(map[uint64]ChunkHandle)}
+	s.ongoing[id] = PartialValue{servers: make(map[uint64]ChunkHandle)}
 	s.m.Unlock()
 	return PreparedWrite{
 		Id: id,
@@ -146,9 +150,16 @@ func (s *Server) RecordChunk(args RecordChunkArgs) {
 // From chunk
 func (s *Server) FinishWrite(args FinishWriteArgs) {
 	s.m.Lock()
-	v := s.ongoing[args.WriteId]
+	v := s.ongoing[args.WriteId].servers
 	// TODO: do we want to forget ongoing writes?
-	s.data[args.Keyname] = v
+
+	numChunks := uint64(len(v))
+	var servers = make([]ChunkHandle, 0)
+	for i := uint64(0); i < numChunks; i++ {
+		servers = append(servers, v[i])
+	}
+
+	s.data[args.Keyname] = Value{servers: servers}
 	s.m.Unlock()
 }
 
@@ -156,12 +167,7 @@ func (s *Server) PrepareRead(keyname string) PreparedRead {
 	s.m.Lock()
 	// need to convert map to slice
 	// (the map should be total because it is in s.data)
-	indexHandleMap := s.data[keyname].servers
-	numChunks := uint64(len(indexHandleMap))
-	var servers = make([]ChunkHandle, 0)
-	for i := uint64(0); i < numChunks; i++ {
-		servers = append(servers, indexHandleMap[i])
-	}
+	servers := s.data[keyname].servers
 	s.m.Unlock()
 	return PreparedRead{Handles: servers}
 }
