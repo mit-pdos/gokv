53de94d61d7f04dd7bea66c33b29bf44ea008162
Nickolai Zeldovich <nickolai@csail.mit.edu>
2023-06-20 22:41:16 -0400

    prove bank for any number of accounts

--- bank/bank.go
+++ bank/bank.go
@@ -5,16 +5,16 @@ import (
 	"github.com/mit-pdos/gokv/connman"
 	"github.com/mit-pdos/gokv/lockservice"
 	"github.com/mit-pdos/gokv/memkv"
+	"github.com/tchajed/goose/machine"
 )
 
-// The maximum money supply, initially will all belong to acc1
+// The maximum money supply, initially will all belong to accts[0]
 const BAL_TOTAL = uint64(1000)
 
 type BankClerk struct {
-	lck  *lockservice.LockClerk
-	kvck *memkv.SeqKVClerk
-	acc1 uint64
-	acc2 uint64
+	lck   *lockservice.LockClerk
+	kvck  *memkv.SeqKVClerk
+	accts []uint64
 }
 
 func acquire_two(lck *lockservice.LockClerk, l1 uint64, l2 uint64) {
@@ -47,14 +47,30 @@ func (bck *BankClerk) transfer_internal(acc_from uint64, acc_to uint64, amount u
 	release_two(bck.lck, acc_from, acc_to)
 }
 
-func (bck *BankClerk) SimpleTransfer(amount uint64) {
-	bck.transfer_internal(bck.acc1, bck.acc2, amount)
+func (bck *BankClerk) SimpleTransfer() {
+	for {
+		src := machine.RandomUint64()
+		dst := machine.RandomUint64()
+		amount := machine.RandomUint64()
+		if src < uint64(len(bck.accts)) && dst < uint64(len(bck.accts)) && src != dst {
+			bck.transfer_internal(bck.accts[src], bck.accts[dst], amount)
+		}
+	}
 }
 
 func (bck *BankClerk) get_total() uint64 {
-	acquire_two(bck.lck, bck.acc1, bck.acc2)
-	sum := memkv.DecodeUint64(bck.kvck.Get(bck.acc1)) + memkv.DecodeUint64(bck.kvck.Get(bck.acc2))
-	release_two(bck.lck, bck.acc1, bck.acc2)
+	var sum uint64
+
+	// For deadlock avoidance, assume bck.accts is sorted
+	for _, acct := range bck.accts {
+		bck.lck.Lock(acct)
+		sum = sum + memkv.DecodeUint64(bck.kvck.Get(acct))
+	}
+
+	for _, acct := range bck.accts {
+		bck.lck.Unlock(acct)
+	}
+
 	return sum
 }
 
@@ -66,18 +82,19 @@ func (bck *BankClerk) SimpleAudit() {
 	}
 }
 
-func MakeBankClerk(lockhost memkv.HostName, kvhost memkv.HostName, cm *connman.ConnMan, init_flag uint64, acc1 uint64, acc2 uint64, cid uint64) *BankClerk {
+func MakeBankClerk(lockhost memkv.HostName, kvhost memkv.HostName, cm *connman.ConnMan, init_flag uint64, accts []uint64, cid uint64) *BankClerk {
 	bck := new(BankClerk)
 	bck.lck = lockservice.MakeLockClerk(lockhost, cm)
 	bck.kvck = memkv.MakeSeqKVClerk(kvhost, cm)
-	bck.acc1 = acc1
-	bck.acc2 = acc2
+	bck.accts = accts
 
 	bck.lck.Lock(init_flag)
 	// If init_flag has an empty value, initialize the accounts and set the flag.
 	if std.BytesEqual(bck.kvck.Get(init_flag), make([]byte, 0)) {
-		bck.kvck.Put(acc1, memkv.EncodeUint64(BAL_TOTAL))
-		bck.kvck.Put(acc2, memkv.EncodeUint64(0))
+		bck.kvck.Put(bck.accts[0], memkv.EncodeUint64(BAL_TOTAL))
+		for _, acct := range bck.accts[1:] {
+			bck.kvck.Put(acct, memkv.EncodeUint64(0))
+		}
 		bck.kvck.Put(init_flag, make([]byte, 1))
 	}
 	bck.lck.Unlock(init_flag)
