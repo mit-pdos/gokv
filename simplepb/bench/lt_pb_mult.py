32ae573f491ffb1a038eda28391d49903d34b834
Upamanyu Sharma <upamanyu@mit.edu>
2023-07-25 18:15:45 -0400

    More merging

--- simplepb/bench/lt_pb_mult.py
+++ simplepb/bench/lt_pb_mult.py
@@ -12,7 +12,7 @@ import atexit
 import signal
 from datetime import datetime
 
-from common import *
+from .goycsb import *
 
 def num_threads(i):
     if i < 10:
@@ -20,63 +20,22 @@ def num_threads(i):
     i = i - 10
     return 100 * (i + 1)
 
-def closed_lt(kvname, warmuptime, runtime, valuesize, outfilename, readprop, updateprop, recordcount, thread_fn):
-    i = 0
-    last_good_index = i
-    peak_thruput = 0
-
-    while True:
-        if i > last_good_index + 5:
-            break
-        threads = thread_fn(i)
-
-        # restart every single time
-        start_kv_system()
-
-        # start_single_core_single_node_kv_system()
-        a = goycsb_bench(kvname, threads, warmuptime, runtime, valuesize, readprop, updateprop, recordcount,
-                         ['-p', f"pbkv.configAddr={config['serverhost']}:12000"])
-        p = {'service': kvname, 'num_threads': threads, 'lts': a}
-
-        with open(outfilename, 'a+') as outfile:
-            outfile.write(json.dumps(p) + '\n')
-
-        thput = sum([ a[op]['thruput'] for op in a ])
-
-        if thput > peak_thruput:
-            last_good_index = i
-        if thput > peak_thruput:
-            peak_thruput = thput
-
-        last_threads = threads
-
-        i = i + 1
-
-    return
-
-def start_kv_system():
+def reset_kv_system():
     os.system("./start-pb.py --ncores 8 3")
 
-config = {}
-
-def main():
-    atexit.register(cleanup_procs)
+def run(outfilepath, readratio, warmuptime=30, runtime=120):
     resource.setrlimit(resource.RLIMIT_NOFILE, (100000, 100000))
-    global config
-
-    readratio = float(global_args.reads)
+    serverhost = '10.10.1.4'
     config = {
-        'read': readratio,
-        'write': 1 - readratio,
-        'keys': 1000,
-        'serverhost': '10.10.1.4',
-        'warmuptime': 10,
-        'runtime': 10,
+        'outfilename': outfilepath,
+        'reads': readratio,
+        'writes': 1 - readratio,
+        'recordcount': 1000,
+        'warmuptime': warmuptime,
+        'runtime': runtime,
+        'valuesize': 128,
     }
 
-    outfilepath = global_args.outfile
-    closed_lt('pbkv', config['warmuptime'], config['runtime'], 128, outfilepath, config['read'], config['write'], config['keys'], num_threads)
+    closed_lt('pbkv', config, reset_kv_system, num_threads,
+              ['-p', f"pbkv.configAddr={serverhost}:12000"])
     cleanup_procs()
-
-if __name__=='__main__':
-    main()
