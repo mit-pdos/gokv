9028c61a3da2f8ef6cbaeb0bdf14bdbbc5453bde
Upamanyu Sharma <upamanyu@mit.edu>
2023-09-05 23:12:14 -0400

    New bank closed example

--- simplepb/apps/closed/mains.go
+++ simplepb/apps/closed/mains.go
@@ -1,37 +1,61 @@
 package closed
 
 import (
+	"github.com/mit-pdos/gokv/bank"
 	"github.com/mit-pdos/gokv/grove_ffi"
+	"github.com/mit-pdos/gokv/lockservice"
 	"github.com/mit-pdos/gokv/simplepb/apps/kv"
 	"github.com/mit-pdos/gokv/simplepb/config"
-	"github.com/tchajed/goose/machine"
 )
 
 const (
-	r1         = uint64(1)
-	r2         = uint64(2)
-	configHost = uint64(10)
+	// data
+	dr1         = uint64(1)
+	dr2         = uint64(2)
+	dconfigHost = uint64(10)
+	// lock
+	lr1         = uint64(101)
+	lr2         = uint64(102)
+	lconfigHost = uint64(110)
 )
 
-func config_main() {
+func lconfig_main() {
 	var servers = make([]uint64, 0)
-	servers = append(servers, r1)
-	servers = append(servers, r2)
-	config.MakeServer(servers).Serve(configHost)
+	servers = append(servers, lr1)
+	servers = append(servers, lr2)
+	config.MakeServer(servers).Serve(lconfigHost)
 }
 
-func kv_replica_main(fname string, me grove_ffi.Address) {
+func dconfig_main() {
+	var servers = make([]uint64, 0)
+	servers = append(servers, dr1)
+	servers = append(servers, dr2)
+	config.MakeServer(servers).Serve(dconfigHost)
+}
+
+func kv_replica_main(fname string, me, configHost grove_ffi.Address) {
 	x := new(uint64)
 	*x = uint64(1)
 	kv.Start(fname, me, configHost)
 }
 
-func kv_client_main(fname string, me grove_ffi.Address) {
-	ck := kv.MakeClerk(configHost)
-	ck.Put("a", "ABCD")
-	v1 := ck.Get("a")
-	machine.Assert(len(v1) == 4)
-	ck.Put("a", "EFG")
-	v2 := ck.Get("a")
-	machine.Assert(len(v2) == 3)
+func makeBankClerk() *bank.BankClerk {
+	kvck := kv.MakeKv(dconfigHost)
+	lck := lockservice.MakeLockClerk(kv.MakeKv(lconfigHost))
+
+	return bank.MakeBankClerk(lck, kvck, "init", "a1", "a2")
+}
+
+func bank_transferer_main() {
+	bck := makeBankClerk()
+	for {
+		bck.SimpleTransfer()
+	}
+}
+
+func bank_auditor_main() {
+	bck := makeBankClerk()
+	for {
+		bck.SimpleAudit()
+	}
 }
